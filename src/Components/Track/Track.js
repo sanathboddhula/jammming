53a3e8e4c4afbcbde346445cae13799246dff998
sanathboddhula <46285560+sanathboddhula@users.noreply.github.com>
2022-07-07 12:36:01 -0400

    Add files via upload

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import "./Track.css";
+
+export class Track extends React.Component {
+  constuctor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  renderAction() {
+    //prop is passed in
+    if (this.props.isRemoval) {
+      //render button with negative symbol
+      return <button className="Track-action">-</button>;
+    } else {
+      //render button with plus symbol
+      return (
+        <button className="Track-action" onClick={this.addTrack}>
+          +
+        </button>
+      );
+    }
+  }
+
+  addTrack() {
+    this.props.onAdd(this.props.track);
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name} </h3>
+          <p>
+            {" "}
+            {this.props.track.artist} | {this.props.track.album}{" "}
+          </p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
+}
+
+export default Track;
